ec364b99e33dba53d79d0a6b8fe13a2d447f3120
Andrey Bel <Belstrel@gmail.com>
2026-04-19 09:55:29 +0300

    Update routes_auth.py

--- auth_service/app/api/routes_auth.py
+++ auth_service/app/api/routes_auth.py
@@ -3,7 +3,7 @@ from typing import Annotated
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 
-from app.api.deps import get_auth_uc, get_current_user_id
+from app.api.deps import get_auth_uc, get_current_user
 from app.schemas.auth import RegisterRequest, TokenResponse
 from app.schemas.user import UserPublic
 from app.usecases.auth import AuthUseCase
@@ -29,7 +29,7 @@ async def login(
 
 @router.get("/me", response_model=UserPublic)
 async def me(
-    user_id: Annotated[int, Depends(get_current_user_id)],
+    user_id: Annotated[int, Depends(get_current_user)],
     uc: Annotated[AuthUseCase, Depends(get_auth_uc)],
 ):
     return await uc.me(user_id)
